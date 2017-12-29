17c3c24a67aaeea4c59f16e6013b1af156c25b6c
joseph <joseph.arenas@entelgy.com>
2017-12-29 18:06:07 -0500

    test facade

--- src/test/java/com/bbva/pzic/proposals/dao/model/hyt6/TestTransaccionHyt6.java
+++ src/test/java/com/bbva/pzic/proposals/dao/model/hyt6/TestTransaccionHyt6.java
@@ -1,62 +1,40 @@
 package com.bbva.pzic.proposals.dao.model.hyt6;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.junit.Ignore;
+import com.bbva.jee.arq.spring.core.host.ServicioTransacciones;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-
-import com.bbva.jee.arq.spring.core.host.ExcepcionTransaccion;
-import com.bbva.jee.arq.spring.core.host.protocolo.ExcepcionRespuestaHost;
-import com.bbva.jee.arq.spring.core.host.transporte.ExcepcionTransporte;
-import com.bbva.jee.arq.spring.core.servicing.test.BusinessServiceTestContextLoader;
-import com.bbva.jee.arq.spring.core.servicing.test.MockInvocationContextTestExecutionListener;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * Test de la transacci&oacute;n <code>HYT6</code>
  * 
  * @author Arquitectura Spring BBVA
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-	loader = BusinessServiceTestContextLoader.class, 
-	locations = {
-        "classpath*:/META-INF/spring/applicationContext-*.xml", 
-        "classpath:/META-INF/spring/business-service.xml",
-        "classpath:/META-INF/spring/business-service-test.xml"
-    }
-)
-@Ignore
-@TestExecutionListeners(listeners = { MockInvocationContextTestExecutionListener.class, DependencyInjectionTestExecutionListener.class })
+@RunWith(MockitoJUnitRunner.class)
 public class TestTransaccionHyt6 {
-	
-	private static final Log LOG = LogFactory.getLog(TestTransaccionHyt6.class);
-		
-	@Autowired
+
+	@InjectMocks
 	private TransaccionHyt6 transaccion;
-	
+
+	@Spy
+	private ServicioTransacciones servicioTransacciones = Mockito.mock(ServicioTransacciones.class);
+
 	@Test
-	public void test() throws ExcepcionTransaccion {
-		
-		PeticionTransaccionHyt6 peticion = new PeticionTransaccionHyt6();		
-		
-		/*
-		 * TODO: poblar la peticion con valores adecuados
-		 */
-		
-		try {
-			LOG.info("Invocando transaccion, peticion: " + peticion);
-			RespuestaTransaccionHyt6 respuesta = transaccion.invocar(peticion);
-			LOG.info("Recibida respuesta: " + respuesta);
-		} catch ( ExcepcionRespuestaHost e ) {
-			LOG.error("Error recibido desde host, codigoError: " + e.getCodigoError() + ", descripcion: " + e.getMessage());
-		} catch ( ExcepcionTransporte e ) {
-			LOG.error("Error de transporte", e);
-		}
+	public void test() {
+
+		PeticionTransaccionHyt6 peticion = new PeticionTransaccionHyt6();
+
+		RespuestaTransaccionHyt6 respuesta = new RespuestaTransaccionHyt6();
+
+		Mockito.when(servicioTransacciones.invocar(PeticionTransaccionHyt6.class, RespuestaTransaccionHyt6.class,
+				peticion)).thenReturn(respuesta);
+
+		RespuestaTransaccionHyt6 result = transaccion.invocar(peticion);
+
+		Assert.assertEquals(result, respuesta);
 	}
 }
